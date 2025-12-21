e26ec5504f142dbfc349d4db2f269880c60ada01
gkaro <37290133+gkaro@users.noreply.github.com>
2025-12-21 13:27:18 +0100

    Add files via upload

---
+++ config/app_local.php
@@ -0,0 +1,112 @@
+<?php
+
+use function Cake\Core\env;
+
+/*
+ * Local configuration file to provide any overrides to your app.php configuration.
+ * Copy and save this file as app_local.php and make changes as required.
+ * Note: It is not recommended to commit files with credentials such as app_local.php
+ * into source code version control.
+ */
+return [
+    /*
+     * Debug Level:
+     *
+     * Production Mode:
+     * false: No error messages, errors, or warnings shown.
+     *
+     * Development Mode:
+     * true: Errors and warnings shown.
+     */
+    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
+
+    /*
+     * Security and encryption configuration
+     *
+     * - salt - A random string used in security hashing methods.
+     *   The salt value is also used as the encryption key.
+     *   You should treat it as extremely sensitive data.
+     */
+    'Security' => [
+        'salt' => env('SECURITY_SALT', '12b6f8d2d8b5dedb59f5820e5284af685d2b65dd22cb0320a479305484b164c3'),
+    ],
+
+    /*
+     * Connection information used by the ORM to connect
+     * to your application's datastores.
+     *
+     * See app.php for more configuration options.
+     */
+    'Datasources' => [
+        'default' => [
+            'host' => 'aikidoh489.mysql.db',
+            /*
+             * CakePHP will use the default DB port based on the driver selected
+             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
+             * the following line and set the port accordingly
+             */
+            //'port' => 'non_standard_port_number',
+
+            'username' => 'aikidoh489',
+            'password' => 'At69mAurzEaq',
+
+            'database' => 'aikidoh489',
+            /*
+             * If not using the default 'public' schema with the PostgreSQL driver
+             * set it here.
+             */
+            //'schema' => 'myapp',
+
+            /*
+             * You can use a DSN string to set the entire configuration
+             */
+            'url' => env('DATABASE_URL', null),
+        ],
+
+        /*
+         * The test connection is used during the test suite.
+         */
+        'test' => [
+            'host' => 'localhost',
+            //'port' => 'non_standard_port_number',
+            'username' => 'my_app',
+            'password' => 'secret',
+            'database' => 'test_myapp',
+            //'schema' => 'myapp',
+            'url' => env('DATABASE_TEST_URL', 'sqlite://127.0.0.1/tmp/tests.sqlite'),
+        ],
+    ],
+
+    /*
+     * Email configuration.
+     *
+     * Host and credential configuration in case you are using SmtpTransport
+     *
+     * See app.php for more configuration options.
+     */
+    'EmailTransport' => [
+        'default' => [
+            'host' => 'localhost',
+            'port' => 25,
+            'username' => null,
+            'password' => null,
+            'client' => null,
+            'url' => env('EMAIL_TRANSPORT_DEFAULT_URL', null),
+        ],
+    ],
+
+    'GoogleMap' => [
+        'googleMapKey' => 'AIzaSyAxoP4zo_UbIDv1A5R8caiRziCDAXJy5jE' 
+    ],
+
+    'HelloAsso' => [
+        'association' => 'ligue-hauts-de-france-aikido-ffab',
+        'helloassoClient' => '97b0e5d593d8439098fba70ca803812a',
+        'helloassoClientSecret' => 'l83FXOv3fLNevCvce6O4QQUjy47qrzX9',
+    ],
+
+    'Email' => [
+        'emailAddress' => 'act@aikido-hdf.fr'
+    ]
+
+];
